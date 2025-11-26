8f688102e9dec662e1a99b74f45b805f74e9e376
davidarana713 <almacenamientoarana@gmail.com>
2025-11-25 21:46:38 -0500

    se agrega las rutas

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const express = require('express');
+const express = require('express'); //Importaciom
 
-const app = express();    
-const PORT = 3000;
+const app = express();       //Invocando core Express
+const PORT = 3000;           //Definiendo el puerto de escucha
 
+//Definir rutas
+app.get('/',( req, res ) =>{
+    res.send('<h1>home</h1>');
+});
+
+app.get('/health',( req, res ) =>{
+    res.send('<h1>health</h1>');
+});
+
+
+//Lanzando el servidor web
 app.listen( PORT, () => {
-    console.log(`Serve running on http:localhots:${PORT}`)
+    console.log(`Serve running on http://localhost:${PORT}`)
 } );
