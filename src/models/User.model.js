ce7dbcaab3c2f1b98de8aebebeb229ed7e8ce202
davidarana713 <almacenamientoarana@gmail.com>
2025-12-01 20:27:42 -0500

    creacion funcional del crod

---
+++ src/models/User.model.js
@@ -0,0 +1,58 @@
+import { Schema, model } from "mongoose";
+
+
+//Creando una instacioa del esquema de la entidad User
+const userSchema = new Schema({
+    username:{
+        //Reglas
+        type:String, //Define el tipo
+        required: true, //Es obligatorio
+        //Modificador
+        unique: true, //obliga a que el valor sea unico
+        trim: true,   //Elimina los espacios en blanco
+        lowercase: true
+    }, //Obliga a que los caracteres sean en minuscula
+         
+        name:{type:String,
+            required: true,
+            trim: true
+        },
+    
+        email:{type:String,
+            required: true,
+            trim:true,
+            lowercase:true,
+            unique:true
+        },
+
+        password: {type:String,
+            required: true,
+            trim:true,
+            minLength: 8,
+            maxLength: 12
+        },
+        role:{
+            type:String,
+            enum:['admin','super-admin','resgister','colaborator','registered'],
+            default:'registered'
+        },
+        isActive:{
+            type:Boolean,
+            default: true
+        },
+        // isVerified:{
+        //     code:String,
+        //     trim: true,
+
+        // }
+
+},{}); 
+
+//Crear el modelo user basado en el esquema userSchema
+const userModel = model(
+    'users', //Nombre de la coleccion en singular 'User'
+    userSchema  //Esquema asociado al modelo
+);
+
+//Eportando el modelo user para que sea usado en otras partes de la aplicacion
+export default userModel;
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,14 +1,22 @@
 //Importando la dependencia 'express' usando Common.js
 import express from 'express'
+import userModel from '../models/User.model.js';
 
 const router = express.Router();
 
 //definicion de las rutas
-router.post( '/', ( req, res ) => {
+router.post( '/', async ( req, res ) => {
     const data = req.body;
+
+    //Mostrar en la consola
+    console.log(data);
+
+    //registrar los datos usando el userModel
     
+    const dataRegister = await userModel.create(data); //Registrar los datos en la base de datos
+
     res.json({ msg: 'Crear un usuario',
-        data:data 
+        dataRegister 
     });
 } );
 
